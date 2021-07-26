a38373fd4c25ba596a9886ba492fa63ff5974cd9
AezardSR <69091066+AezardSR@users.noreply.github.com>
2021-07-26 13:09:40 +0200

    modif

---
+++ NotreSucré.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>Notre Sucré</title>
+</head>
+<body>
+<header>
+    <div id="mainNav">
+        <div>
+            <a href="index.html"><img id="logoNav" src="asset/img/LogoPN01-300x265.png" alt=""></a>
+        </div>
+        <nav id="menu" class="toggle-on">
+            <a id="pres" href="presentation.html">Pascal Noppe</a>
+            <a href="index.html" >Accueil</a>
+            <a href="NotreSucré.php" class="active">Notre Sucré</a>
+            <a href="#">Notre Salé</a>
+            <a href="#">Contact</a>
+        </nav>
+        <div id="menu-button" class="close"></div>
+    </div>
+</header>
+<script src="asset/js/main.js"></script>
+</body>
+</html>
